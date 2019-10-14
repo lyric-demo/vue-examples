586985cb9c2e2b6dc760fc0a5c489332a253c639
lyric <tiannianshou@gmail.com>
2019-10-14 08:47:28 +0800

    add vue ant design demo

--- 03_vuex/src/store/modules/counter.js
+++ 03_vuex/src/store/modules/counter.js
@@ -2,21 +2,21 @@ const counter = {
   namespaced: true,
   state() {
     return {
-      count: 0,
+      count: 0
     };
   },
   mutations: {
     increment(state) {
       state.count++;
-    },
+    }
   },
   actions: {
     increment({ commit }) {
       setTimeout(() => {
-        commit('increment');
+        commit("increment");
       }, 100);
-    },
-  },
+    }
+  }
 };
 
 export default counter;
